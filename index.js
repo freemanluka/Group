df057c564c7faca773d31a04a0efb67933177e42
FreemanLuka <freemanluka@gmail.com>
2024-04-25 14:39:01 +0100

    this is my first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+function sumTwoDigits (dit1, dit2) {
+    return dit1 + dit2;
+}
+
+console.log(sumTwoDigits(2, 3));
---
+++ myGroup.js
@@ -0,0 +1,7 @@
+const firstName = "Barnabas"
+
+const lastName = "Bulus"
+
+const fullName = firstName + " " + lastName
+
+console.log(fullName)
